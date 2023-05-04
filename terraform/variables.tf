14a090f5b1612c6e59c5caa1685f2fb0400a552c
anmutetech <124485136+anmutetech@users.noreply.github.com>
2023-05-03 23:28:24 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,7 @@ variable "instance_type" {
 default = "t2.micro"
 }
 variable "instance_key" {
-default = "anmute-ec2-key"
+default = "<replace with your existing key pair"
 }
 variable "vpc_cidr" {
 default = "178.0.0.0/16"
